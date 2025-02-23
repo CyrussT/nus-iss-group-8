0033f8f04f6a5f65574bd5220b1f5b584c265850
cyrus <24277152+CyrussT@users.noreply.github.com>
2025-02-23 22:15:58 +0800

    Refactor auth store to use cookies for token management and improve user state initialization and added admin and student Auth guard

---
+++ rbs-frontend/pages/unauthorized.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+  <div class="flex flex-col items-center justify-center min-h-screen">
+    <div class="p-8 rounded-lg shadow-md">
+      <h1 class="text-3xl font-bold mb-4 ml-4">Unauthorized Access</h1>
+      <p class="mb-6">You do not have permission to access this page.</p>
+      <div class="flex justify-center">
+        <NuxtLink 
+          to="/login" 
+          class="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded"
+        >
+          Back to Login
+        </NuxtLink>
+      </div>
+    </div>
+  </div>
+</template>
