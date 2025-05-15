d75a172e6e9e78021fb5a9a5e24a6f0a9520f8b6
cyrus <24277152+CyrussT@users.noreply.github.com>
2025-05-15 22:01:56 +0800

    Reordered DAST Scan and URL

--- scripts/zap-script.sh
+++ scripts/zap-script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 docker pull zaproxy/zap-stable
-docker run -i zaproxy/zap-stable zap-baseline.py -t "https://github.com/CyrussT/nus-iss-group-8" -l PASS > zap_baseline_report.html
+docker run -i zaproxy/zap-stable zap-baseline.py -t "http://146.190.102.157/" -l PASS > zap_baseline_report.html
 
 echo $? > /dev/null
