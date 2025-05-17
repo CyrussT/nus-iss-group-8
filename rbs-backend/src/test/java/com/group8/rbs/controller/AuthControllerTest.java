eccb6d4330258b345304962f5a6d143ba80707f1
Walterlayzz <walterlayzz@gmail.com>
2025-05-17 21:12:19 +0800

    Removal of unused import

--- rbs-backend/src/test/java/com/group8/rbs/controller/AuthControllerTest.java
+++ rbs-backend/src/test/java/com/group8/rbs/controller/AuthControllerTest.java
@@ -14,7 +14,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
